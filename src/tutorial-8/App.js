22ed1b90d66638606abbd2986ef1a77c675957cf
Denis Berezin <nocab139@yandex.ru>
2021-11-16 22:52:03 +0300

    ДЗ №8. метод post, отправка данных на сервер

--- src/tutorial-8/App.js
+++ src/tutorial-8/App.js
@@ -15,6 +15,23 @@ const App = () => {
         }
     };
 
+     const createUsers = () => {
+         const emailElem = document.querySelector('#email')
+         const usernameElem = document.querySelector('#username')
+         const data = {
+             email: emailElem.value,
+             name: usernameElem.value
+         }
+         fetch('https://6193cd7d221e680017450c22.mockapi.io/users', {
+             method: 'POST',
+             body: JSON.stringify(data),
+             headers: {
+                 Accept: 'application/json',
+                 'Content-Type': 'application/json',
+             }
+         })
+     }
+
     return (
         <>
             <ul>
@@ -25,6 +42,15 @@ const App = () => {
                 ))}
             </ul>
             <button onClick={getUsers}>Получить список пользователей</button>
+
+            <hr/>
+            <br/>
+            <div>
+                <h4>Создать пользователя:</h4>
+                <input id="email" type="text" placeholder="Email" />
+                <input id="username" type="text" placeholder="Имя" />
+                <button onClick={createUsers}>Добавить</button>
+            </div>
         </>
     );
 }
